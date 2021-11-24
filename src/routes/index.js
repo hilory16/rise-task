6deabba61239120e59a3231843fd2064428b2195
Olamide Ilori <olamide.ilori@getprospa.com>
2021-11-24 23:07:05 +0100

    Initial setup

---
+++ src/routes/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Landing from 'pages/Landing';
+
+export default function Index() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
